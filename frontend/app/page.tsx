1dc712c92307945ed91df4c46fb1777fba680a20
silvered-shark <n.melo1013@gmail.com>
2025-12-02 15:30:45 +0100

    feat: Update first page

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -5,6 +5,7 @@ import { useRouter } from "next/navigation";
 import { LoadingCtx } from "@/context/loadingContext";
 import Loading from "@/components/loading";
 import { UserInfoCtx } from "@/context/userContext";
+import Image from "next/image";
 
 const Home = () => {
   const { loadingState } = useContext(LoadingCtx);
@@ -25,7 +26,25 @@ const Home = () => {
   }
 
   if (loading === "success") {
-    return <div className="bg-white min-h-screen"></div>;
+    return (
+      <div className="bg-white min-h-screen">
+        <div className="container mx-auto">
+          <div className="relative">
+            <div className="absolute top-60 left-0 w-full h-full">
+              <div className="flex flex-col items-center justify-center gap-6">
+                <div className="w-50 h-50 rounded-full overflow-hidden">
+                  <Image src="/Grmps/grmps.jpg" alt="Grmps" width={100} height={100} className="w-full h-full object-cover" />
+                </div>
+                <div className="flex flex-col items-center justify-center gap-2">
+                  <h1 className="text-display font-bold text-[#7E3FF2]">Welcome to Grumpus</h1>
+                  <p className="text-normal font-regular text-[#7E3FF2]">The platform for freelancers and clients (Beta Version)</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
   } else {
     return <Loading />;
   }
