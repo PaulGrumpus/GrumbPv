74214ddd39de38d20e6de4fc6fcb46b04645d5c7
silvered-shark <n.melo1013@gmail.com>
2025-12-03 15:01:15 +0100

    feat: chat navbar

---
+++ frontend/components/chatNavbar.tsx
@@ -0,0 +1,36 @@
+import Button from "./button";
+import Image from "next/image";
+
+const ChatNavbar = () => {
+    return (
+        <div className="py-4 px-16">
+            <div className="container mx-auto">
+                <div className="flex justify-between items-center">
+                    <h1 className="text-title font-bold text-black">Chat</h1>
+                    <div className="flex items-center gap-2">
+                        <Button padding="p-2">
+                            <Image 
+                                src="/Grmps/search.svg" 
+                                alt="Search" 
+                                width={24} 
+                                height={24} 
+                                className="h-full w-full object-cover"
+                            />
+                        </Button>
+                        <Button padding="p-2">
+                            <Image 
+                                src="/Grmps/three-dots.svg" 
+                                alt="Three Dots" 
+                                width={24} 
+                                height={24} 
+                                className="h-full w-full object-cover"
+                            />
+                        </Button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ChatNavbar;
