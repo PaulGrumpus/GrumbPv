076192424a4087b578c7dcc5a4e503a1bc20fa57
silvered-shark <n.melo1013@gmail.com>
2025-12-16 02:03:38 +0100

    feat: job and gig

--- frontend/app/jobs/page.tsx
+++ frontend/app/jobs/page.tsx
@@ -57,11 +57,11 @@ const JobsPage = () => {
     
     return (
         <div>
-            <div className="px-16 bg-white pt-46">
+            <div className="lg:px-16 px-4 bg-white lg:pt-46 pt-22">
                 <div className="container mx-auto">
-                    <p className="text-display font-bold text-black pb-6">Jobs</p>
+                    <p className="lg:text-display text-title lg:text-left text-center font-bold text-black pb-6">Jobs</p>
                     <p className="text-normal font-regular text-black pb-20">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique.</p>
-                    <div className="grid grid-cols-2 gap-8 pb-28">  
+                    <div className="grid lg:grid-cols-2 grid-cols-1 gap-8 pb-28">  
                         {jobs.map((job) => (
                             <PubJobOrGigPost 
                                 key={job.id} 
